47683da415cd70fc18a645793d6f3e490b898f19
Levente Szabó <szabo.levente.dev@gmail.com>
2022-04-20 08:26:31 +0200

    addBorder is done

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,45 @@
-export function addBorder(picture: string[]): string[] {
+import { toNamespacedPath } from "path";
 
+export function addBorder(picture: string[]): string[] {
+    let result:string[] = [];
+    let maxLength:number = 0;
+    
+    //check the length of the longest element in the array
+    picture.forEach(element => {
+        if(element.length> maxLength) maxLength = element.length;
+        return maxLength;
+    })
+    
+    let tempStr:string;
+    //add the upper part of the frame
+    tempStr = ''
+    for(let i:number = 0; i< maxLength+2; i++){
+        tempStr += '*';
+    }
+    result.push(tempStr);
+    
+    
+    //add the inner part of the picture
+    picture.map((line) => {
+        let diff:number = maxLength - line.length;
+        
+        tempStr = '';
+        
+        if(diff == 0){
+            tempStr = '*' + line + '*';
+            result.push(tempStr);
+        }
+        
+    })
+    
+    //add the bottom part of the frame
+    tempStr = ''
+    for(let i:number = 0; i< maxLength+2; i++){
+        tempStr += '*';
+    }
+    result.push(tempStr);    
+    console.log(result);
+    return result;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+ console.log(addBorder(["abc", "ded"]));
