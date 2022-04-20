1b1eb2aab3d1ad53c17b493a4bd3e03828cdcde6
Levente Szabó <szabo.levente.dev@gmail.com>
2022-04-20 13:27:50 +0200

    centuryFromYear() method is done

--- centuryFromYear/centuryFromYear.ts
+++ centuryFromYear/centuryFromYear.ts
@@ -1,6 +1,6 @@
 export function centuryFromYear(year: number): number {
-
+    return Math.floor(year/100)+1;
 }
 
-// console.log(centuryFromYear(1905));
-// console.log(centuryFromYear(1700));
+ console.log(centuryFromYear(1905));
+ console.log(centuryFromYear(1700));
