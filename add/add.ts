a2f95d848e27606bc5de8a1bef260035d924e7f9
Levente Szabó <szabo.levente.dev@gmail.com>
2022-04-20 07:31:38 +0200

    add and add2 fininshed

--- add/add.ts
+++ add/add.ts
@@ -1,12 +1,23 @@
 export function add(param1: number, param2: number): number {
-
+    return param1 + param2
 }
 
 export function add2(...param1: number[]): number {
-
+   /* let final:number = param1.forEach(value => {
+        let result:number = 0;
+        result += value;
+        return value;
+    })  
+    return final; */
+    let result:number = 0;
+    for (let index = 0; index < param1.length; index++) {
+        const element = param1[index];
+        result += element;
+    }
+    return result;
 }
-// console.log(add(1, 2));
+console.log(add(1, 2));
 // console.log(add(3, 2));
 
-// console.log(add2(1,2,3,4,5));
+console.log(add2(1,2,3,4,5));
 // console.log(add2(2,3));
